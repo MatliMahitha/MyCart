7d1f48454433ce74b4878afca2feb8ab1063b270
MatliMahitha <matlimahita@gmail.com>
2022-06-29 22:33:49 +0530

    init commit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,24 +2,45 @@ import React from 'react'
 
 function Cart(props) {
   const { cartItems, decQuantity, incQuantity } = props
-  console.log('Cart Items', cartItems.length)
-  let sum = cartItems.reduce((a, b) => a + b.price * b.quantity, 0)
+
+  let sum = cartItems.reduce((a, b) =>
+    a + b.price * b.quantity, 0)
+    
   return (
     <div >
       <h1>Cart ({cartItems.length})</h1>
       {
         cartItems.map((cartItem, i) => {
           return <div key={i}>
-            <img className="small" src={cartItem.image} alt={cartItem.name}></img>
-            <h3>{cartItem.name}</h3>
-            <div >{`Rs.${cartItem.price * cartItem.quantity}`}</div>
-            <button onClick={() => decQuantity(cartItem)}>-</button> {cartItem.quantity}
-            <button onClick={() => incQuantity(cartItem)}>+</button>
-            
+            <img
+              className="small"
+              src={cartItem.image}
+              style={{ width: 100, height: 70 }}
+              alt={cartItem.name}>
+            </img>
+            <h3> {cartItem.name} {cartItem.quantity}kg </h3>
+            <div> {`Rs.${cartItem.price * cartItem.quantity}`} </div>
+            <button
+              onClick={() => decQuantity(cartItem)}>
+              -
+            </button>
+            {cartItem.quantity}
+            <button
+              onClick={() => incQuantity(cartItem)}>
+              +
+            </button>
+            <hr />
+
           </div>
         })
       }
-      { sum === 0 ? <h4>Cart is empty</h4> :<h4>TotalPrice = {sum}</h4>}
+      <hr />
+      {
+        sum === 0 ?
+          (<h4>Cart is empty</h4>)
+          :
+          (<h4>TotalPrice = Rs.{sum}</h4>)
+      }
     </div>
   )
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,5 @@
 import { React, useState } from 'react'
-import { Button, Col, Container, Row, CardGroup, Card } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 import swal from 'sweetalert'
 import Cart from './Cart'
 
@@ -28,15 +28,29 @@ function Product() {
     },
     {
       id: '4',
-      name: 'potatoes',
+      name: 'potatos',
       price: 14,
       quantity: 5,
       image: "https://www.bing.com/th?id=ODL.c83081dc3ebb938451a5d2ee3fcede29&w=197&h=112&c=7&rs=1&qlt=80&o=6&dpr=2&pid=RichNav"
     },
+    {
+      id: '5',
+      name: 'carrot',
+      price: 46,
+      quantity: 5,
+      image: "https://www.bing.com/th?id=OIP.4C3v3ExBEap1_9TF92vTSgHaEo&w=316&h=197&c=8&rs=1&qlt=90&o=6&dpr=2&pid=3.1&rm=2"
+    },
+    {
+      id: '6',
+      name: 'beetroot',
+      price: 45,
+      quantity: 5,
+      image: "https://www.bing.com/th?id=OIP.spsKkO6D7VXRI6CBxchthgHaE1&w=309&h=201&c=8&rs=1&qlt=90&o=6&dpr=2&pid=3.1&rm=2"
+    },
   ])
 
   const alertMe = (product) => {
-    swal(`${product.name } - 'Out Of Stock'` )
+    swal(`${product.name} - 'Out Of Stock'`)
   }
 
   products.map((product) => {
@@ -47,22 +61,17 @@ function Product() {
   const [select, setSelect] = useState(1)
 
   const addProduct = (product) => {
-    const result = cartItems.filter(item => item.id === product.id)
-      if (product.quantity >= select) {
-        if (result.length > 0) {
-          setCartItems(cartItems.map(item => item.id === result[0].id ?
-            { ...item, quantity: item.quantity + Number(select) } : { ...item }))
-        } else {
-          setCartItems([...cartItems, { ...product, quantity: Number(select) }])
-        }
-      }
-      if (result.length > 0) {
-        setCartItems(cartItems.map(item => item.id === result[0].id ?
-          { ...item, quantity: item.quantity + Number(select) } : { ...item }))
-      } else {
-        setCartItems([...cartItems, { ...product, quantity: Number(select) }])
-      }
-      decTotalQuantity(product, select)
+    const result = cartItems.filter(item =>
+      item.id === product.id)
+    if (result.length > 0) {
+      setCartItems(cartItems.map(item =>
+        item.id === product.id ?
+          { ...item, quantity: item.quantity + Number(select) } :
+          { ...item }))
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: Number(select) }])
+    }
+    decTotalQuantity(product, select)
   }
 
   const decTotalQuantity = (selectedProduct, selectedQuantity) => {
@@ -75,54 +84,68 @@ function Product() {
     }))
   }
 
-  const decQuantity = (product) => {
-    const result = cartItems.find(it => it.id === product.id)
+  const decQuantity = (cartItem) => {
+    const result = cartItems.find(it =>
+      it.id === cartItem.id)
     if (result.quantity === 1) {
-      setCartItems(cartItems.filter(fi => fi.id !== product.id))
+      setCartItems(cartItems.filter(fi =>
+        fi.id !== cartItem.id))
     } else {
-      setCartItems(cartItems.map(item => item.id === product.id ?
-        { ...item, quantity: item.quantity - 1 } : { ...item }))
+      setCartItems(cartItems.map(item =>
+        item.id === cartItem.id ?
+          { ...item, quantity: item.quantity - 1 } :
+          { ...item }))
     }
+
   }
 
   const handleChange = (e, product) => {
-    console.log('Target Value', e.target.value)
     setSelect(e.target.value)
   }
 
   return (
     <Container className="shadow mb-5 p-2 rounded" >
-      <Row>
-        <Col xs={12} md={8}  >
-          <h1>Products</h1>
-          {
-            products.map((product, i) => {
-              return <div key={i} className="m-2">
-                <CardGroup>
-                <Card style={{ width: '1rem' }}>
-                  <Card.Img variant="top" src={product.image} />
-                  <Card.Body>
-                    <Card.Title>{product.name}</Card.Title>
-                    <Card.Text>
-                      {`Rs. ${product.price * select}`}
-                    </Card.Text>
-                    <select className="m-2" onChange={(e) => { handleChange(e, product) }} >
-                      <option value="1">{`1kg-Rs.${product.price}`}</option>
-                      <option value="2">{`2kg-Rs.${product.price * 2}`}</option>
-                      <option value="3">{`3kg-Rs.${product.price * 3}`}</option>
-                      <option value="4">{`4kg-Rs.${product.price * 4}`}</option>
-                      <option value="5">{`5kg-Rs.${product.price * 5}`}</option>
-                    </select>
-                    <Button variant="outline-primary" onClick={() => { addProduct(product) }}>Add to Cart</Button>
-                  </Card.Body>
-                </Card>
-                </CardGroup>
-              </div>
-            })
-          }
+      <Row >
+        <Col xs={12} md={7}  >
+          <h1>PRODUCTS</h1>
+          <div class="row row-cols-1-hover row-cols-md-3 g-4">
+            {
+              products.map((product, i) => {
+                return <div key={i} class="col">
+                  <div key={i} class="card-group">
+                    <div class="card h-800">
+                      <img src={product.image}
+                        style={{ width: 197, height: 100 }}
+                        class="card-img-top"
+                        alt={product.name}>
+                      </img>
+                      <div class="card-body">
+                        <h5 class="card-title"> {product.name} </h5>
+                        <p class="card-text"> {`Rs. ${product.price * select}`} </p>
+                        <select className="m-2" onChange={(e) => { handleChange(e, product) }} >
+                          <option value="1">{`1kg-Rs.${product.price * 1}`}</option>
+                          <option value="2">{`2kg-Rs.${product.price * 2}`}</option>
+                          <option value="3">{`3kg-Rs.${product.price * 3}`}</option>
+                          <option value="4">{`4kg-Rs.${product.price * 4}`}</option>
+                          <option value="5">{`5kg-Rs.${product.price * 5}`}</option>
+                        </select>
+                        <Button
+                          variant="outline-primary"
+                          onClick={() => { addProduct(product) }}>
+                          Add
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              })
+            }
+          </div>
         </Col>
         <Col xs={6} md={4}>
-          <Cart cartItems={cartItems} decQuantity={decQuantity} incQuantity={addProduct} />
+          <Cart cartItems={cartItems}
+            decQuantity={decQuantity}
+            incQuantity={addProduct} />
         </Col>
       </Row>
     </Container>
